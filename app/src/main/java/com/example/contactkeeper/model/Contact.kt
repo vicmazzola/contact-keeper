3282995183004834928dbaf1707d0e5f956eaad7
Victor Mazzola <victormazzola2019@gmail.com>
2025-03-12 18:31:08 -0300

    feat: implement ContactsViewModel and Contact data model

---
+++ app/src/main/java/com/example/contactkeeper/model/Contact.kt
@@ -0,0 +1,7 @@
+package com.example.contactkeeper.model
+
+data class Contact(
+    val name: String,
+    val phone: String,
+    val isFriend: Boolean
+)
--- app/src/main/java/com/example/contactkeeper/viewmodel/ContactsViewModel.kt
+++ app/src/main/java/com/example/contactkeeper/viewmodel/ContactsViewModel.kt
@@ -1,4 +1,27 @@
 package com.example.contactkeeper.viewmodel
 
-class ContactsViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import com.example.contactkeeper.model.Contact
+
+class ContactsViewModel : ViewModel() {
+    var name by mutableStateOf("")
+    var phone by mutableStateOf("")
+    var isFriend by mutableStateOf(false)
+    var contactsList by mutableStateOf(listOf<Contact>())
+
+    fun addContact() {
+        if (name.isNotEmpty() && phone.isNotEmpty()) {
+            contactsList = contactsList + Contact(name, phone, isFriend)
+            name = ""
+            phone = ""
+            isFriend = false
+        }
+    }
+
+    fun deleteContact(contact: Contact) {
+        contactsList = contactsList - contact
+    }
 }
