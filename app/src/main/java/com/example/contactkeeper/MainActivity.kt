01d3dc6efbf7c18977259be46f469748bca46280
Victor Mazzola <victormazzola2019@gmail.com>
2025-03-13 07:13:13 -0300

    refactor: removed MVVM separation, moved all logic to MainActivity

--- app/src/main/java/com/example/contactkeeper/MainActivity.kt
+++ app/src/main/java/com/example/contactkeeper/MainActivity.kt
@@ -3,41 +3,215 @@ package com.example.contactkeeper
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.contactkeeper.ui.ContactsScreen
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.contactkeeper.ui.theme.ContactKeeperTheme
-import com.example.contactkeeper.viewmodel.ContactsViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             ContactKeeperTheme {
-                Surface(modifier = Modifier.fillMaxSize()) {
-                    val viewModel: ContactsViewModel = viewModel()
-                    ContactsScreen(viewModel)
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column {
+                        ContactsScreen()
+                    }
                 }
             }
         }
     }
 }
 
-
-@Preview(
-    showSystemUi = true,
-    showBackground = true
-)
 @Composable
-fun ContactsScreenPreview() {
-    ContactKeeperTheme {
-        val viewModel = ContactsViewModel()
-        ContactsScreen(viewModel)
+fun ContactsScreen() {
+    var nameState = remember {
+        mutableStateOf("")
+    }
+
+    var phoneState = remember {
+        mutableStateOf("")
+    }
+
+    var friendState = remember {
+        mutableStateOf(false)
+    }
+
+    Column {
+        ContactForm(
+            name = nameState.value,
+            phone = phoneState.value,
+            friend = friendState.value,
+            onNameChange = {
+                nameState.value = it
+            },
+            onPhoneChange = {
+                phoneState.value = it
+            },
+            onFriendChange = {
+                friendState.value = it
+            }
+        )
+        ContactList()
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ContactForm(
+    name: String,
+    phone: String,
+    friend: Boolean,
+    onNameChange: (String) -> Unit,
+    onPhoneChange: (String) -> Unit,
+    onFriendChange: (Boolean) -> Unit
+) {
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Text(
+            text = "Contact Registration",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(
+                0xFFE91E63
+            )
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        OutlinedTextField(
+            value = name,
+            onValueChange = { onNameChange(it) },
+            modifier = Modifier.fillMaxWidth(),
+            label = {
+                Text(text = "Contact Name")
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                capitalization = KeyboardCapitalization.Words
+            )
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        OutlinedTextField(
+            value = phone,
+            onValueChange = { onPhoneChange(it) },
+            modifier = Modifier.fillMaxWidth(),
+            label = {
+                Text(text = "Contact Phone")
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Phone
+            )
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Checkbox(checked = friend, onCheckedChange = {
+                onFriendChange(it)
+            })
+            Text(text = "Friend")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                text = "REGISTER",
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun ContactList() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .verticalScroll(rememberScrollState())
+    ) {
+        for (i in 0..10) {
+            ContactCard()
+            Spacer(modifier = Modifier.height(4.dp))
+        }
+    }
+}
+
+@Composable
+fun ContactCard() {
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.LightGray
+        )
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .weight(2f)
+            ) {
+                Text(
+                    text = "Contact Name",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = "8888-9999",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = "Friend",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    imageVector = Icons.Default.Delete,
+                    contentDescription = ""
+                )
+            }
+        }
     }
 }
