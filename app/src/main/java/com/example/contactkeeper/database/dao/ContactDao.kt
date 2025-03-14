3a67b7852b34de3f604d4ea930214685800d6db3
Victor Mazzola <victormazzola2019@gmail.com>
2025-03-14 09:26:47 -0300

    feat: complete ContactDao with Room annotations

--- app/src/main/java/com/example/contactkeeper/database/dao/ContactDao.kt
+++ app/src/main/java/com/example/contactkeeper/database/dao/ContactDao.kt
@@ -1,14 +1,27 @@
 package com.example.contactkeeper.database.dao
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.contactkeeper.model.Contact
+
+@Dao
 interface ContactDao {
 
-    fun insertContact()
+    @Insert
+    fun insertContact(contact: Contact): Long
 
-    fun updateContact()
+    @Update
+    fun updateContact(contact: Contact): Int
 
-    fun deleteContact()
+    @Delete
+    fun deleteContact(contact: Contact): Int
 
-    fun getContactById()
+    @Query("SELECT * FROM tb_contact WHERE id = :idContact")
+    fun getContactById(idContact: Long): Contact
 
-    fun getAllContacts()
+    @Query("SELECT * FROM tb_contact ORDER BY name ASC")
+    fun getAllContacts(): List<Contact>
 }
